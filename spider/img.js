9197b5e56a301191ec18c35e85e09533885bb1e2
L IN <linzb93@users.noreply.github.com>
2019-04-22 23:12:34 +0800

    修改http请求模块为axios

--- spider/img.js
+++ spider/img.js
@@ -1,37 +1,29 @@
-//依赖模块
-    var fs = require('fs');
-    var request = require('request');
-    var cheerio = require("cheerio");
-    var mkdirp = require('mkdirp');
-    //目标网址
-    var url = 'http://www.baidu.com';
-    //本地存储目录
-    var dir = './bdimages';
-    //创建目录
-    mkdirp(dir, function(err) {
-        if(err){
-            console.log(err);
-        }
-    });
-    //发送请求
-    request(url, function(error, response, body) {
-        if(response.statusCode == 200) {
-            var $ = cheerio.load(body);
-            $('img').each(function() {
-                var src =$(this).attr('src');
-                if (src.substr(0,5) === '//www') {
-                    src = 'http:' + src;
-                }
-                console.log('正在下载' + src);
-                var picName = $(this).attr('src').split('/').pop();
-                download(src, dir, picName);
-                console.log('下载完成');
-            });
-        }
-    });
-    //下载方法
-    var download = function(url, dir, filename){
-        request.head(url, function(err, res, body){
-            request(url).pipe(fs.createWriteStream(dir + "/" + filename));
-        });
-    };
+var fs = require('fs');
+const axios = require('axios');
+var cheerio = require("cheerio");
+var mkdirp = require('mkdirp');
+//目标网址
+var page = 'http://www.baidu.com';
+//本地存储目录
+var dir = './bdimages';
+//创建目录
+mkdirp(dir, function(err) {
+    if(err){
+        console.log(err);
+    }
+});
+//发送请求
+axios.get(page)
+.then(html => {
+    const $ = cheerio.load(html.data);
+    $('img').each(function() {
+        const filename = $(this).attr('src').split('/').slice(-1)[0];
+        const url = 'http:' + $(this).attr('src');
+        axios.get(url, {
+            responseType: 'stream'
+        })
+        .then(imgRes => {
+            imgRes.data.pipe(fs.createWriteStream(`${dir}/${filename}`))
+        })
+    })
+})
