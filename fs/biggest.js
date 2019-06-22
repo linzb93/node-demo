33047fb0a6bf7f04f8d26824e7ac539e36ce0202
L IN <linzb93@users.noreply.github.com>
2019-06-22 23:19:32 +0800

    递归查看最大的文件/文件夹

--- fs/biggest.js
+++ fs/biggest.js
@@ -1,84 +1,131 @@
+/**
+* 递归获取当前目录下最大的文件/文件夹
+*/
 const fs = require('fs');
 const path = require('path');
 const bytes = require('bytes');
 const chalk = require('chalk');
-const root = '';
+const pify = require('pify');
+const open = require('open');
+const inquirer = require('inquirer');
+const biggerSize = '1GB';
+const root = ''; // 请输入你需要查询的文件夹路径
 
-// 简单实现一个promisify
-function promisify(fn) {
-  return (...args) => (
-    new Promise((resolve, reject) => {
-      args.push((...cbArgs) => {
-        // 做error first兼容处理
-        let err, result;
-        if (cbArgs.length === 2) {
-          err = cbArgs[0];
-          result = cbArgs[1];
-        } else if (cbArgs.length === 1) {
-          result = cbArgs[0];
-        }
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-      fn.apply(null, args);
-    })
-  )
+const readdir = pify(fs.readdir);
+const stat = pify(fs.stat);
+
+// 获取最大项
+function getMaxItem(items, key) {
+  let maxItem = items[0];
+  items.forEach(item => {
+    if (item[key] > maxItem[key]) {
+      maxItem = item;
+    }
+  });
+  return maxItem;
 }
 
-const readdir = promisify(fs.readdir);
-const stat = promisify(fs.stat);
+// 判断该路径下是否全是文件
+function isAllFileInDir(absDir) {
+  const dirs = fs.readdirSync(absDir);
+  return dirs.every(dir => !fs.lstatSync(path.resolve(absDir, dir)).isDirectory());
+}
 
 // 递归搜索文件
 function readFileRecursive(dir, callback) {
   return readdir(dir)
-    .then(files => {
-      const promiseFilesList = files.map(item => {
-        const dest = path.join(dir, item);
-        return stat(dest)
-          .then(stats => {
-            if (stats.isDirectory()) {
-              return readFileRecursive(dest, callback);
-            }
-            callback(dest);
-          }).catch(err => {
-            console.log(err);
-          });
-      })
-      return Promise.all(promiseFilesList);
+  .then(files => {
+    const promiseFilesList = files.map(item => {
+      const dest = path.join(dir, item);
+      return stat(dest)
+      .then(stats => {
+        if (stats.isDirectory()) {
+          return readFileRecursive(dest, callback);
+        }
+        callback(dest);
+      });
     })
-    .catch(err => {
-      console.log(err);
-    });
+    return Promise.all(promiseFilesList);
+  });
 }
 
 function getDirSize(roots) {
   const list = [];
-  readFileRecursive(roots, dest => {
+  return readFileRecursive(roots, dest => {
     list.push(dest);
   })
-    .then(() => {
-      const size = list.reduce((a, b) => {
-        return a + fs.statSync(b).size;
-      }, 0);
-      const transformedSize = bytes(size, { decimalPlaces: 1 });
-      const output = size >= bytes('1GB') ? chalk.red(transformedSize) : transformedSize;
-      console.log(`${path.basename(roots)}: ${output}`);
+  .then(() => {
+    const size = list.reduce((a, b) => {
+      return a + fs.statSync(b).size;
+    }, 0);
+    return Promise.resolve({
+      name: path.basename(roots),
+      isDirectory: true,
+      size
     });
+  });
 }
 
-readdir(root)
+function loop(absDir) {
+  console.log('\n');
+  console.log('=================================');
+  console.log(`进入${absDir}文件夹`);
+  readdir(absDir)
   .then(files => {
-    files.forEach(file => {
-      stat(path.resolve(root, file))
-        .then(stats => {
-          if (stats.isDirectory()) {
-            getDirSize(path.resolve(root, file));
-          } else {
-            console.log(`${path.basename(file)}: ${bytes(stats.size, { decimalPlaces: 1 })}`);
-          }
-        })
-    })
+    const pm = files.map(file => {
+      return stat(path.resolve(absDir, file))
+      .then(stats => {
+        if (stats.isDirectory()) {
+          return getDirSize(path.resolve(absDir, file));
+        } else {
+          return Promise.resolve({
+            name: path.basename(file),
+            isDirectory: false,
+            size: stats.size
+          });
+        }
+      })
+    });
+    return Promise.all(pm);
   })
+  .catch(err => {
+    if (err.code === 'ENOENT') {
+      console.log(chalk.red('文件夹路径错误，请重新输入'));
+    } else {
+      console.log(err);
+    }
+  })
+  .then(resList => {
+    resList.forEach(res => {
+      const transformedSize = bytes(res.size, { decimalPlaces: 1 });
+      const output = res.size >= bytes(biggerSize) ? chalk.red(transformedSize) : transformedSize;
+      console.log(`${res.isDirectory ? '文件夹' : '文件'} ${res.name}: ${output}`);
+    });
+    const maxItem = getMaxItem(resList, 'size');
+    console.log(chalk.green(`最大的${maxItem.isDirectory ? '文件夹' : '文件'}是${maxItem.name}, 尺寸是${bytes(maxItem.size, {decimalPlaces: 1})}`));
+    if (!isAllFileInDir(absDir)) {
+      inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'toDeeper',
+          message: '是否继续深度搜索？',
+          default: false
+        }
+      ])
+      .then(answer => {
+        if (answer.toDeeper) {
+          loop(path.resolve(absDir, maxItem.name))
+        } else {
+          console.log(chalk.green('检索完成，退出程序！'));
+          open(absDir);
+        }
+      })
+    } else {
+      console.log('\n');
+      console.log(chalk.green('检索完成，退出程序！'));
+      open(absDir);
+    }
+  });
+}
+
+loop(root);
