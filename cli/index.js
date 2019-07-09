7051d399234204b10aca5ab8cb5b131e910b383d
L IN <linzb93@qq.com>
2019-07-09 14:21:21 +0800

    添加多个包的比较

--- cli/index.js
+++ cli/index.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
+const meow = require('meow');
 
-const program = require('commander');
+const cli = meow();
 
-program
-.option('npm-s, --npm-search <name>');
+const type = cli.input[0];
+const arg = cli.input.slice(1);
 
-program.parse(process.argv);
-
-if (program.npmSearch) {
-  require('./npmSearch')(program.npmSearch);
+switch(type) {
+  case 'mikit':
+    require('./mikit')();
+    break;
+  case 'npm-s':
+    require('./npmSearch')(arg);
+    break;
+  default:
+    console.log('命令输入有误，请重新输入！');
+    break;
 }
--- cli/npmSearch.js
+++ cli/npmSearch.js
@@ -4,6 +4,10 @@ const open = require('open');
 const inquirer = require('inquirer');
 const ora = require('ora');
 const chalk = require('chalk');
+const Table = require('cli-table3');
+const table = new Table({
+  head: [chalk.green('名称'), chalk.green('周下载量')]
+});
 
 function transformNumberCn(val) {
   // 12,345,678 => 1234万
@@ -14,53 +18,83 @@ function transformNumberCn(val) {
   return value;
 }
 
-const fetchNp = async packageName => {
-  let spinner = ora(`正在查找 ${packageName} 模块`).start();
-  let res;
-  try {
-    res = await axios.get(`https://www.npmjs.com/package/${packageName}`);
-  } catch (e) {
-    if (e.response && e.response.statusText === 'Not Found') {
-      spinner.fail(`没有 ${packageName} 这个模块`);
-    } else if (e.code === 'ETIMEDOUT') {
-      spinner.fail(`访问超时`);
-    } else {
-      spinner.fail(`无法访问`);
+const fetchNp = async args => {
+  if (args.length === 1) {
+    const packageName = args[0];
+    let spinner = ora(`正在查找 ${packageName} 模块`).start();
+    let res;
+    try {
+      res = await axios.get(`https://www.npmjs.com/package/${packageName}`);
+    } catch (e) {
+      if (e.response && e.response.statusText === 'Not Found') {
+        spinner.fail(`没有 ${packageName} 这个模块`);
+      } else if (e.code === 'ETIMEDOUT') {
+        spinner.fail(`访问超时`);
+      } else {
+        spinner.fail(`无法访问`);
+      }
+      process.exit(0);
     }
-  }
-  spinner.stop();
-  const $ = cheerio.load(res.data);
-  if (!$('._9ba9a726').length) {
-    console.log(chalk.red(`可能 https://npmjs.com 重新发布，请修改。`));
-    return;
-  }
-  
-  /**
-  * 获取描述文字
-  * 如果第一段是徽章，就获取第二段，否则移除第一段里面的html标签
-  */
-  const $firstP = $('article p').first();
-  const data = {
-    desc: $firstP.text().trim() === '' ? $firstP.next().text() : $firstP.text(),
-    weeklyDl: transformNumberCn($('._9ba9a726').text()),
-    lastPb: $('.f2874b88 time').text()
-  };
-  console.log('查找完成');
-  console.log(`${chalk.bold(`关于${packageName}`)}:
+    spinner.stop();
+    const $ = cheerio.load(res.data);
+    if (!$('._9ba9a726').length) {
+      console.log(chalk.red(`可能 https://npmjs.com 重新发布，请修改。`));
+      return;
+    }
+
+    /**
+    * 获取描述文字
+    * 如果第一段是徽章，就获取第二段，否则移除第一段里面的html标签
+    */
+    const $firstP = $('article p').first();
+    const data = {
+      desc: $firstP.text().trim() === '' ? $firstP.next().text() : $firstP.text(),
+      weeklyDl: transformNumberCn($('._9ba9a726').text()),
+      lastPb: $('.f2874b88 time').text()
+    };
+    console.log(`${chalk.bold(`关于${packageName}`)}:
   ${data.desc}
   周下载量：${chalk.green(data.weeklyDl)}
   上次更新：${chalk.green(data.lastPb)}`);
-  const {openHomepage} = await inquirer.prompt([
-    {
-      type: 'confirm',
-      name: 'openHomepage',
-      message: '是否打开Github主页？',
-      default: false
+    const { openHomepage } = await inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'openHomepage',
+        message: '是否打开Github主页？',
+        default: false
+      }
+    ]);
+    if (openHomepage) {
+      const url = $('.fdbf4038').children().eq(6).find('a').attr('href');
+      open(url);
     }
-  ]);
-  if (openHomepage) {
-    const url = $('.fdbf4038').children().eq(6).find('a').attr('href');
-    open(url);
+  } else {
+    let spinner = ora(`正在查找 ${args.join(' ')} 这些模块`).start();
+    let resList;
+    try {
+      resList = await Promise.all(args.map(arg => axios.get(`https://www.npmjs.com/package/${arg}`)));
+    } catch(e) {
+      if (e.response && e.response.statusText === 'Not Found') {
+        spinner.fail(`没有 ${e.config.url.split('/').pop()} 这个模块`);
+      } else {
+        spinner.fail(`无法访问`);
+      }
+      process.exit(0);
+    }
+    spinner.stop();
+    const retList = resList.map(res => {
+      const $ = cheerio.load(res.data);
+      if (!$('._9ba9a726').length) {
+        console.log(chalk.red(`可能 https://npmjs.com 重新发布，请修改。`));
+        process.exit(0);
+      }
+      let obj = {};
+      const key = res.config.url.split('/').pop();
+      obj[key] = transformNumberCn($('._9ba9a726').text());
+      return obj;
+    });
+    table.push(...retList)
+    console.log(table.toString())
   }
 }
 
