dcb48fc98e30dea29974fab6965a3381d44254e8
L IN <linzb93@qq.com>
2019-10-14 09:24:56 +0800

    新增压缩的

--- markdown/parser.js
+++ markdown/parser.js
@@ -6,13 +6,15 @@ const pify = require('pify');
 const MarkdownIt = require('markdown-it');
 const cheerio = require('cheerio');
 const chalk = require('chalk');
-
+const ora = require('ora');
 const md = new MarkdownIt();
 const readFile = pify(fs.readFile);
 
+const spinner = ora('正在解析文档').start();
 readFile('', 'utf8')
 .then(res => {
   const $ = cheerio.load(md.render(res));
+  spinner.stop();
   const itemLength = $('ul').slice(1).find('li').length;
   console.log(`共有${chalk.yellow(itemLength)}个npm包`);
 });
