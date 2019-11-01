bf5b15e4185759a83ff42bd556f9b1a26c206949
L IN <linzb93@qq.com>
2019-11-01 11:33:17 +0800

    更新

--- spider/net.js
+++ spider/net.js
@@ -1,7 +1,7 @@
 // 检测网址是否可以访问
 const axios = require('axios');
 const cheerio = require('cheerio');
-const pAll = require('p-all');
+const pMap = require('p-map');
 const ProgressBar = require('progress');
 const iconvLite = require('iconv-lite');
 const chalk = require('chalk');
@@ -34,27 +34,26 @@ axios.get('http://www.nd.com.cn/about/link.shtml', {
     cliCursor.hide();
     console.log('\n');
     const start = new Date().getTime();
-    await pAll(siteList.map(item => {
-      return async () => {
-        try {
-          await axios.get(item.url);
-          bar.tick();
-        } catch (e) {
-          bar.tick();
-          errorCounter.count++;
-          let errMsg = '';
-          if (e.response && e.response.statusText === 'Not Found') {
-            errMsg = `${item.title} 网站不存在`;
-          } else if (e.code === 'ETIMEDOUT') {
-            errMsg = `${item.title} 访问超时`;
-          } else {
-            errMsg = `${item.title} 无法访问`;
-          }
-          errorCounter.site.push(errMsg);
-          bar.interrupt(chalk.red(errMsg));
+    await pMap(siteList, async item => {
+      try {
+        await axios.get(item.url);
+        bar.tick();
+      } catch (e) {
+        bar.tick();
+        errorCounter.count++;
+        let errMsg = '';
+        if (e.response && e.response.statusText === 'Not Found') {
+          errMsg = `${item.title} 网站不存在`;
+        } else if (e.code === 'ETIMEDOUT') {
+          errMsg = `${item.title} 访问超时`;
+        } else {
+          errMsg = `${item.title} 无法访问`;
         }
+        errorCounter.site.push(errMsg);
+        bar.interrupt(chalk.red(errMsg));
       }
-    }));
+      return Promise.resolve();
+    })
     cliCursor.show();
     console.log('\n');
     console.log(`用时${ms(new Date().getTime() - start)}`);
