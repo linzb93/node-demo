eaa7334425e95d33b2f5f65d7146928f0b514af8
linzb <linzb93@qq.com>
2020-09-01 09:15:45 +0800

    把命令行的代码分离出去

--- cli/mock/index.js
+++
@@ -1,5 +0,0 @@
-const shell = require('shelljs');
-
-module.exports = () => {
-  shell.exec('cd /Users/linzhibin/Documents/GitHub/easymock && npm run prod');
-}
--- cli/yuque/index.js
+++
@@ -1,3 +0,0 @@
-const {moduleInit} = require('../lib/util');
-
-module.exports = moduleInit.bind(this, __dirname);
