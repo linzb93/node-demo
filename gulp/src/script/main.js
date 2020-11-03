d6e72eae2cf937d1abdab54d6276f680ccd62840
linzb <linzb93@qq.com>
2020-11-03 12:52:49 +0800

    删除无用的文件

--- gulp/src/script/main.js
+++
@@ -1,4 +0,0 @@
-function ab(a) {
-    var b2;
-    return b2 = a === 42 ? 2 : b2 + 2;
-}
--- mock/index.js
+++
@@ -1,9 +0,0 @@
-var Mock = require('mockjs');
-var data = Mock.mock({
-    'list|1-10': [{
-        'id': '@integer(1, 120)',
-        'email': '@email(163.com)'
-    }]
-});
-// 输出结果
-console.log(data);
